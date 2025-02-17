a40ee10e85300590b3d3d064b08c3013e667086c
Shivam Rai <116290534+Nyxoy77@users.noreply.github.com>
2025-02-17 20:14:11 +0530

    Finished setting up credentials for oauth2

--- authservice/oAuth_login.go
+++ authservice/oAuth_login.go
@@ -1,6 +1,9 @@
 package authservice
 
 import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 )
@@ -11,4 +14,11 @@ var googleOAuthConfig = &oauth2.Config{
 	RedirectURL:  "https://localhost:8081/user/oauth/callback",
 	Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
 	Endpoint:     google.Endpoint,
+}
+
+var oauthStateString = "random_string"
+
+func HandleGoogleLogin(c *gin.Context) {
+	url := googleOAuthConfig.AuthCodeURL(oauthStateString, oauth2.AccessTypeOffline)
+	c.Redirect(http.StatusTemporaryRedirect, url)
 }
